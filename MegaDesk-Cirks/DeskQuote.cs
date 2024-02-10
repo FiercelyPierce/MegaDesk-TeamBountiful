f5208fd5172ffacdaf131c21a44ad40abe08a780
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2024-02-09 21:01:17 -0700

    Calculations running from the rushOrderPrices.txt file

--- MegaDesk-Cirks/DeskQuote.cs
+++ MegaDesk-Cirks/DeskQuote.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
 
 namespace MegaDesk_Cirks
 {
@@ -10,6 +12,8 @@ namespace MegaDesk_Cirks
         public Desk Desk { get; set; }
         public int RushDays { get; set; }
         public decimal QuotePrice { get; set; }
+
+        public static List<DeskQuote> _quotes = new List<DeskQuote>(); //Quote List
 
         public DeskQuote() { }
 
@@ -22,9 +26,8 @@ namespace MegaDesk_Cirks
             QuotePrice = CalculateQuotePrice(desk, rushDays);
         }
 
-        private static int[,] ReadRushOrderPrices()
+        private static int[,] ReadRushOrderPrices(string[] lines)
         {
-            string[] lines = System.IO.File.ReadAllLines("rushOrderPrices.txt");
             int[,] prices = new int[3, 3];
             int row = 0;
             for (int i = 0; i < lines.Length; i += 3)
@@ -41,8 +44,10 @@ namespace MegaDesk_Cirks
         // Calculate Quote Price
         public static decimal CalculateQuotePrice(Desk desk, int rushDays)
         {
+            string[] lines = File.ReadAllLines("rushOrderPrices.txt");
+            int[,] rushPrices = ReadRushOrderPrices(lines);
+
             decimal quotePrice = 200;
-            int[,] rushPrices = ReadRushOrderPrices();
 
             // Add surface area cost
             int surfaceArea = desk.Width * desk.Depth;
@@ -74,23 +79,28 @@ namespace MegaDesk_Cirks
                     break;
             }
 
-            // Add rush order cost based on the rushOrderPrices.txt file
-            string[] lines = System.IO.File.ReadAllLines("rushOrderPrices.txt");
-            List<string> rushOrderPrices = new List<string>();
-            foreach (string line in lines)
-            {
-                rushOrderPrices.Add(line);
-            }
-
             // Determine rush order price based on rushDays and desk size
             int sizeCategory = desk.Width * desk.Depth <= 1000 ? 0 : desk.Width * desk.Depth <= 2000 ? 1 : 2;
-            int rushCategory = rushDays == 3 ? 0 : rushDays == 5 ? 1 : 2;
-            int rushOrderPrice = rushPrices[sizeCategory, rushCategory];
+            int rushCategory = rushDays == 3 ? 0 : rushDays == 5 ? 1 : rushDays == 7 ? 2 : 3;
 
-            // Add rush order price to quotePrice
-            quotePrice += rushOrderPrice;
+            if (rushCategory == 3)
+            {
+                return quotePrice;
+            }
+            else
+            {
+                int rushOrderPrice = rushPrices[rushCategory, sizeCategory];
 
-            return quotePrice;
+                // Add rush order price to quotePrice
+                quotePrice += rushOrderPrice;
+
+                return quotePrice;
+            }
+        }
+
+        public static List<DeskQuote> GetAllQuotes()  //method to return all quotes
+        {
+            return _quotes;
         }
     }
 }
